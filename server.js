7accea9b65d6447cae36b11aa461acd803d971d4
Saydy01 <saydy01@gmail.com>
2025-06-17 15:46:58 -0700

    router

--- server.js
+++ server.js
@@ -6,5 +6,10 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'hello world' });
 });
 
+// 新加 / 路由
+app.get('/', (req, res) => {
+  res.send('服务已启动，欢迎访问！');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
